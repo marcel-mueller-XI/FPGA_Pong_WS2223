1f02f5b6cfbaf8df971bd622dcc72bff342d7998
marcel-mueller-XI <119355141+marcel-mueller-XI@users.noreply.github.com>
2022-12-23 17:39:01 +0100

    added enitity Control

--- code/Control.vhd
+++ code/Control.vhd
@@ -1,1 +1,15 @@
 -- Control
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity Control is
+port(
+	button_start : in std_logic;
+	button_resetGame : in std_logic;
+	ball_out_ofField : in std_logic;
+	
+	reset_score : out std_logic;
+	start_ball : out std_logic;
+);
+end entity Control;
