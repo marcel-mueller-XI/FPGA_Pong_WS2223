c4ed5bf2bb5f3d1cf5d72a6553c2c65b12256e96
marcel-mueller-XI <119355141+marcel-mueller-XI@users.noreply.github.com>
2023-02-16 03:17:14 +0100

    testbench enhanced

--- code/Control_tb.vhd
+++ code/Control_tb.vhd
@@ -41,7 +41,7 @@ port map(
 	clk_tb <= not(clk_tb) after PERIOD/2;
 	
    button_start <= '0', '1' after 110 ns, '0' after 150 ns, 								'1' after 410 ns, '0' after 430 ns, '1' after 490 ns, '0' after 510 ns;
-   button_resetMatch <= '0', 					'1' after 190 ns, '0' after 230 ns, 														'1' after 530 ns, '0' after 550 ns, '1' after 610 ns, '0' after 630 ns, '1' after 690 ns, '0' after 800 ns, '1' after 810 ns, '0' after 830 ns, '1' after 890 ns, '0' after 910 ns;
+   button_resetMatch <= '0', 					'1' after 190 ns, '0' after 230 ns, 			'1' after 490 ns, '0' after 510 ns,							'1' after 530 ns, '0' after 550 ns, '1' after 610 ns, '0' after 630 ns, '1' after 690 ns, '0' after 800 ns, '1' after 810 ns, '0' after 830 ns, '1' after 890 ns, '0' after 910 ns;
    ball_outOfField <= '0', 								'1' after 290 ns, '0' after 330 ns,						'1' after 450 ns, '0' after 470 ns;
    score_max <= '0',  								'1' after 250 ns, '0' after 270 ns, 						'1' after 450 ns, '0' after 510 ns;
 	
